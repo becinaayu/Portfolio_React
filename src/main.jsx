77588631e5a936b6f7b9c8fb3c954a9c66806519
becinaayu <rm554044@fiap.com.br>
2024-05-02 11:02:15 -0300

    configurando deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.render(
-  <BrowserRouter basename="/">
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+  </React.StrictMode>,
+)
